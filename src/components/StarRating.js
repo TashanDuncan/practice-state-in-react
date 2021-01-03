146cf12099e3ad346339d01a1f92bb3e0e6873f3
viewtifult <tashanduncan1994@gmail.com>
2021-01-03 20:54:35 +0000

    Render stars to page

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -4,10 +4,22 @@ import Star from './Star';
 class StarRating extends Component {
 
   // Initialize a 'rating' state
-
+  state = {
+    rating: 0
+  }
 
   // Write a function that returns 5 Star components
+  renderStars = () => {
+    const totalStars = []
+    const starValue = 5;
 
+    for (let i = 0; i < starValue; i++){
+      totalStars.push(<Star />)
+    }
+
+    return totalStars;
+
+  }
 
   // Write an event handler that updates the rating state.
   // Pass the function to a Star component via props
@@ -17,6 +29,7 @@ class StarRating extends Component {
     return (
       <ul className="course--stars">
         {/* Render the Star components */}
+        {this.renderStars()}
       </ul>
     );
   }
